971878ed91f1edb0508c6b4533e3754887773b56
mdyaseenahmed <mdyaseenahmed1@gmail.com>
2020-09-20 18:47:39 +0530

    Updates

---
+++ precache-manifest.00d6f63d8b8c94b00a81ce38e229a88a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec78256ee54f64c1968081927cfe064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e86f1bffc838e9df66d",
+    "url": "/static/css/2.ddfacf9b.chunk.css"
+  },
+  {
+    "revision": "685324496cd2204f809a",
+    "url": "/static/css/main.fcb7d7e6.chunk.css"
+  },
+  {
+    "revision": "5e86f1bffc838e9df66d",
+    "url": "/static/js/2.9e78b3a5.chunk.js"
+  },
+  {
+    "revision": "bdec40dfd00a910c635003d4918d1035",
+    "url": "/static/js/2.9e78b3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685324496cd2204f809a",
+    "url": "/static/js/main.1ff98335.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "eebe2925f207297484556345114b5eec",
+    "url": "/static/media/DS.eebe2925.png"
+  },
+  {
+    "revision": "8878f7f4f28ad040bcc900c7b9081901",
+    "url": "/static/media/banner-1.8878f7f4.png"
+  },
+  {
+    "revision": "c0120b1720334d6c54700fe580268a77",
+    "url": "/static/media/bootstrap4.c0120b17.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4e3786f1b291bc6d132dfe9a32a40672",
+    "url": "/static/media/linux.4e3786f1.png"
+  },
+  {
+    "revision": "17ecacbd5dc1bf4ed8e6bd24bc3f5001",
+    "url": "/static/media/materialize.17ecacbd.png"
+  },
+  {
+    "revision": "500e2458a630b9a6843a56572c1a89d3",
+    "url": "/static/media/netlify.500e2458.png"
+  },
+  {
+    "revision": "e5e5504443488c184996b5f6f7c66a99",
+    "url": "/static/media/php.e5e55044.png"
+  },
+  {
+    "revision": "c4372e1cd0ad4c852bf8b3febea21608",
+    "url": "/static/media/yaseen.c4372e1c.jpeg"
+  }
+]);
